e774c9561960192d3d015bd23c4d7fdb25dc5f05
Tung Bui <tungbq.info@gmail.com>
2021-11-20 23:39:17 +0700

    [js_oop] sub_classes

---
+++ js-oop/7_subclasses.js
@@ -0,0 +1,25 @@
+class Book {
+	constructor(title, author, year) {
+		this.title = title;
+		this.author = author;
+		this.year = year;
+	}
+
+	getSummary() {
+		return `${this.title} was written by ${this.author} in ${this.year}`;
+	}
+}
+
+// Magazine subclass
+class Magazine extends Book {
+	constructor(title, author, year, month) {
+		super(title, author, year);
+		this.month = month;
+	}
+}
+
+// Instantiate Magazine
+const mag1 = new Magazine('Mag One', 'Lampard', 1990, 'November');
+
+console.log(mag1);
+console.log(mag1.getSummary());
